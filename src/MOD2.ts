8b6ed131ece0c7b5f8f5416bf2fd937e720662fc
vinicirnne <viniciuscirne7@gmail.com>
2025-09-27 11:01:50 -0300

    Finalização no menu e melhorias na classe Estoque

--- src/MOD2.ts
+++ src/MOD2.ts
@@ -42,33 +42,37 @@ class Estoque{
         this.produto.push(produto)
         console.log(`Produtos Cadastrados: `)
         this.produto.forEach((produto, index)=> {
-            console.log(`Produto: ${index + 1}: ${produto.nome}| ${produto.codigo}|${produto.preco}`)
+            console.log(`Produto: ${index + 1}: ${produto.nome}|ID:  ${produto.codigo}| Preço: ${produto.preco}`)
         });
     }
     buscarProduto(nome: string): Produto | undefined{
         return this.produto.find(p => p.getnome() === nome)
     }
-relatorio(produto: Produto):void{
-    const multiplicador = produto.preco;
-    console.log(`Relatorio de Estoque: ${produto.nome}| ${produto.getqtd()}| ${produto.preco}| Preço total = ${produto.preco * multiplicador}`)
+relatorio():void{
+ this.produto.forEach(produto => {
+    console.log(`Produto: ${produto.nome} | Qtds: ${produto.getqtd()} | Preço: ${produto.preco}`); 
+ })
 }
 }
 
 class Entrada extends Movimentacao{
 registrarAlteracao(produto: Produto, quantidade: number): void {
- if(quantidade <= 0){
-    throw new Error(`Não se pode adicionar 0 ou menos`);
- }else{
-    produto.adicionarQuantidade(quantidade);
-    console.log(`Entrada: ${quantidade} ${produto.getnome} -> Estoque ${produto.getqtd}`) 
- }
-}
+   produto.adicionarQuantidade(quantidade);
+    console.log(`Entrada: ${produto.nome} -> Adicionado ${quantidade}`)
 
 }
+}
 
+class Saida extends Movimentacao{
+    registrarAlteracao(produto: Produto, quantidade: number): void {
+        produto.removerQuantidade(quantidade);
+        console.log(`Saida: ${produto.nome} -> Removidas ${quantidade}`);
+    }
+}
 
+const estoque = new Estoque();
 
-/* console.log("===Menu de Opções===");
+console.log("===Menu de Opções===");
 console.log("1 - Cadastrar Produto");
 console.log("2 - Registrar Entrada");
 console.log("3 - Registrar Saida");
@@ -76,8 +80,6 @@ console.log("4 - Gerar relatorio de estoque");
 console.log("5 - Fechar Programa");
 
 do{
-
-let produto: Produto| null = null
 
 const opcao = Number(prompt("Qual operação deseja realizar: "));
 if(opcao < 1 || opcao > 5){
@@ -90,18 +92,32 @@ const nome = prompt("Nome do Produto: ");
 const codigo = prompt("Codigo do produto: ");
 const preco = Number(prompt("Preço do produto: "));
 const quantidade = Number(prompt("quantidade adicionada: "));
-produto = new Produto(nome, codigo, preco, quantidade);
+const produto = new Produto(nome,codigo,preco,quantidade);
+estoque.gerenciarProduto(produto);
 }
 if(opcao == 2){
-const add = Number(prompt("Quantidade a ser adicionada: "));
-const e = new Entrada(produto!);
-produto!.registrar(add);
+const nome  =prompt("Nome do produto para registro: ");
+const produto = estoque.buscarProduto(nome);
+if(produto){
+    const entrada = Number(prompt("Quantidade de entrada: "));
+    new Entrada().registrarAlteracao(produto,entrada);
 }
+}
+if(opcao == 3){
+const nome = prompt("Nome do produto para saida: ");
+const produto = estoque.buscarProduto(nome);
+if(produto){
+    const saida = Number(prompt("Quantidade da saida: "));
+    new Saida().registrarAlteracao(produto,saida)
+}
+}
+if(opcao == 4){
+    estoque.relatorio();
+}
+} while(true);
 
-} while(true); */
-
-const p1 = new Produto("Sabonete liquido", "3", 10, 10)
+/* const p1 = new Produto("Sabonete liquido", "3", 10, 10)
 const E = new Entrada();
 E.registrarAlteracao(p1,10);
 const e1 = new Estoque
-e1.relatorio(p1);
+e1.relatorio(p1); */
